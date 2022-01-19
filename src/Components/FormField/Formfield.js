7caa5c34903704a854b9df27016aa213a003f802
Zen 0ne <gor.sharoyan95@gmail.com>
2022-01-20 00:44:11 +0400

    :art:UI(components/FormField):worked on components UI

--- src/Components/FormField/Formfield.js
+++ src/Components/FormField/Formfield.js
@@ -2,22 +2,29 @@ import React from "react";
 
 //UI
 import TextField from "@mui/material/TextField";
+import "./FormField.css";
 
 export default function FormField({ onChange }) {
   return (
-    <div>
-      <TextField
-        type="text"
-        placeholder="name"
-        name="name"
-        onChange={onChange}
-      />
-      <TextField
-        type="email"
-        placeholder="email"
-        name="email"
-        onChange={onChange}
-      />
+    <div className="formField">
+      <div className="input">
+        <TextField
+          variant="filled"
+          type="text"
+          placeholder="name"
+          name="name"
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <TextField
+          variant="filled"
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={onChange}
+        />
+      </div>
     </div>
   );
 }

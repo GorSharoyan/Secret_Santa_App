6a5767e23bc653075e22efd1c0b8112b67c45c75
Zen 0ne <gor.sharoyan95@gmail.com>
2022-01-25 01:05:00 +0400

    :green_heart:CI(components/SecretSantaMessage.js):created separate component that will illustrate the massage thath player rescieves whit the name of a present resciever

---
+++ src/Components/SecretSantaMessage/SecretSantaMessage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SecretSantaMessage({ sender, resciever }) {
+  return (
+    <div>
+      <h1>Congratulations!!!</h1>
+      <h2>Dear {sender}</h2>
+      <p>
+        We are happy to inform you that you are a {resciever}'s secret Santa
+        this year!!! Merry Christmas and Happy New Year!!!
+      </p>
+    </div>
+  );
+}

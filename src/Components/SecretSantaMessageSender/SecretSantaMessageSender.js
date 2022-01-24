99c0ff21f1ac310805b55a80db4beb33513ed8bc
Zen 0ne <gor.sharoyan95@gmail.com>
2022-01-25 01:14:04 +0400

    :green_heart:CI(components/SecretSantaMessageSender.js):created separate component that in future will render all the neccessery oinformation about the email and further send it to the players

---
+++ src/Components/SecretSantaMessageSender/SecretSantaMessageSender.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ({ resciver }) {
+  return (
+    <div>
+      <form action={`https://formsubmit.co/${resciver.email}`} method="POST">
+        <input type="text" name="name" required></input>
+        <input type="email" name="email" required></input>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}

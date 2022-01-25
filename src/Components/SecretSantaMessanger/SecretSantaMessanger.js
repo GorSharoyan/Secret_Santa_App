8da2aaa09d8ca7c80487902f5bb68662daf1a3f4
Zen 0ne <gor.sharoyan95@gmail.com>
2022-01-26 01:04:43 +0400

    :package:NPM(libs/firebase):Added firebase to the project

--- src/Components/SecretSantaMessanger/SecretSantaMessanger.js
+++ src/Components/SecretSantaMessanger/SecretSantaMessanger.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function SecretSantaMessanger({ resciver }) {
+export default function SecretSantaMessanger() {
+  const [players, setPlayers] = useState([]);
+  useEffect(() => {
+    const playersList = JSON.parse(localStorage.getItem("playersList"));
+    console.log(playersList);
+  }, []);
   return (
     <div>
-      <form action={`https://formsubmit.co/${resciver.email}`} method="POST">
+      <form action={`https://formsubmit.co/$`} method="POST">
         <input type="text" name="name" required></input>
         <input type="email" name="email" required></input>
         <button type="submit">Send</button>

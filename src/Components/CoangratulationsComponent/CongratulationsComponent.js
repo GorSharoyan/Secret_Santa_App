9fb5ab9738653e5c1727fb2baf7225917d6271db
Zen 0ne <gor.sharoyan95@gmail.com>
2022-02-17 23:33:43 +0400

    :green_heart:CI(components/CongratulationComponent):created congratulations component for further integration with the relevant page

---
+++ src/Components/CoangratulationsComponent/CongratulationsComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function CongratulationsComponent() {
+  return (
+    <div>
+      <h1>Congratulations</h1>
+      <p>
+        Secret Santas are on there way! Everyone has been emailed a message with
+        their Secret Santa. Please check your email for your Secret Santa. Merry
+        Christmas and Happy New Year!
+      </p>
+    </div>
+  );
+}

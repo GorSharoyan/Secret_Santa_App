2d65cc4877c30bdb1612d52f5454d6ccf0c75793
Zen 0ne <gor.sharoyan95@gmail.com>
2022-02-18 01:06:12 +0400

    :green_heart:CI(components/LandingMessage):created separate component for landing message

---
+++ src/Components/LendingMessage/LendingMessage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function LandingMessage() {
+  const today = new Date();
+  const newYear = today.getFullYear() + 1;
+  return (
+    <div>
+      <h2>
+        Huraaaaaay!
+        <br></br>
+        {newYear} is almost here! Its time wake up the Santa!
+      </h2>
+      <h4>Please select the number of players</h4>
+    </div>
+  );
+}
--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -6,17 +6,14 @@ import "./LandingPage.css";
 //components
 import LandingAsset from "../../Components/LandingAsset/LandingAsset";
 import PlayerSelectionInput from "../../Components/PlayerSelectionInput/PlayerSelectionInput";
+import LandingMessage from "../../Components/LendingMessage/LendingMessage";
 
 export default function PlayingPage() {
   const today = new Date();
   const newYear = today.getFullYear() + 1;
   return (
     <div className="page">
-      <h2>
-        Huraaaaaay!
-        <br></br>
-        {newYear} is almost here! Its time wake up the Santa!
-      </h2>
+      <LandingMessage />
       <PlayerSelectionInput />
       <LandingAsset />
     </div>

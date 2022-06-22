927700005dae558a70b6efdaa118a6399193a653
Zen 0ne <gor.sharoyan95@gmail.com>
2022-06-23 00:06:46 +0400

    :arrow_up:NPM(materialUI/icons):updated dependencies thus changed Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,12 +4,23 @@ import { makeStyles } from "@mui/styles";
 //UI
 import "./Footer.css";
 
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 export default function Footer() {
   return (
     <div className="root">
-      <ul className="container">
+      <ul className="contacts-bar">
         <li>© Gor Sharoyan 2022 Beta</li>
         <li>gor.sharoyan95@gmail.com</li>
+      </ul>
+      <ul className="social-media-bar">
+        <li>
+          <FacebookIcon />
+        </li>
+        <li>
+          <LinkedInIcon />
+        </li>
       </ul>
     </div>
   );
